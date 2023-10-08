0ad33148e6fbd4a777156668ee53ef9cef03ad43
William Fischdick <jwfbit@hotmail.com>
2023-10-08 19:41:49 -0300

    :construction: Implementando a lógica do jogo.

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 "use strict";
 
+const secretNumber = Math.trunc(Math.random() * 20) +1;
+document.querySelector(".number").textContent = secretNumber;
+let score = 20;
+
 document.querySelector(".check").addEventListener("click", function() {
     const guess = Number(document.querySelector(".guess").value);
 
     if(!guess) {
+
         document.querySelector(".message").textContent = "No Number!";
+
+    } else if (guess === secretNumber) {
+
+        document.querySelector(".message").textContent = "Correct Number!";
+
+    } else if (guess > secretNumber) {
+
+        document.querySelector(".message").textContent = "Too High!";
+        score--;
+        document.querySelector(".score").textContent = score;
+
+    } else if (guess < secretNumber) {
+        
+        document.querySelector(".message").textContent = "Too Low!";
+        score--;
+        document.querySelector(".score").textContent = score;
     }
 });
