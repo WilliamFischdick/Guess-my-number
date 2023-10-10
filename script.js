476b2b803a6c719c6b775994834db59aa384f637
William Fischdick <jwfbit@hotmail.com>
2023-10-10 20:26:18 -0300

    :construction: Implementando a lógica do jogo caso a pontuação seja igual a zero.

--- script.js
+++ script.js
@@ -16,17 +16,24 @@ document.querySelector(".check").addEventListener("click", function() {
         document.querySelector(".message").textContent = "Correct Number!";
 
     } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector(".message").textContent = "Too High!";
+            score--;
+            document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContent = "You Lost the Game!";
+            document.querySelector(".score").textContent = 0;
+        }
 
-        document.querySelector(".message").textContent = "Too High!";
-        score--;
-        document.querySelector(".score").textContent = score;
 
     } else if (guess < secretNumber) {
-        
-        document.querySelector(".message").textContent = "Too Low!";
-        score--;
-        document.querySelector(".score").textContent = score;
+        if (score > 1) {
+            document.querySelector(".message").textContent = "Too Low!";
+            score--;
+            document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContent = "You Lost the Game!";
+            document.querySelector(".score").textContent = 0;
+        }
     }
 });
-
-// adicionar novas funcionalidades.
