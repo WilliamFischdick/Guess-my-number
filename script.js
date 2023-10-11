d1ef79e7c377a69d8ad5b346a047013db775f34a
William Fischdick <jwfbit@hotmail.com>
2023-10-11 20:03:24 -0300

    :construction: Alterando elementos de estilo quando se vence o jogo.

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ document.querySelector(".check").addEventListener("click", function() {
     } else if (guess === secretNumber) {
 
         document.querySelector(".message").textContent = "Correct Number!";
+        document.querySelector("body").style.backgroundColor = "#60b347";
+        document.querySelector(".number").style.width = "30rem";
 
     } else if (guess > secretNumber) {
         if (score > 1) {
