0a96fcbc00c92f6aab3571ae42b2150025f8e70e
William Fischdick <jwfbit@hotmail.com>
2023-10-12 21:32:38 -0300

    :construction: Implementando a função do botão AGAIN do jogo.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) +1;
-document.querySelector(".number").textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) +1;
 let score = 20;
+let highScore = 0
 
 document.querySelector(".check").addEventListener("click", function() {
     const guess = Number(document.querySelector(".guess").value);
@@ -16,6 +16,7 @@ document.querySelector(".check").addEventListener("click", function() {
         document.querySelector(".message").textContent = "Correct Number!";
         document.querySelector("body").style.backgroundColor = "#60b347";
         document.querySelector(".number").style.width = "30rem";
+        document.querySelector(".number").textContent = secretNumber;
 
     } else if (guess > secretNumber) {
         if (score > 1) {
@@ -38,4 +39,15 @@ document.querySelector(".check").addEventListener("click", function() {
             document.querySelector(".score").textContent = 0;
         }
     }
+});
+
+document.querySelector(".again").addEventListener("click", function() {
+    document.querySelector("body").style.backgroundColor = "#222";
+    document.querySelector(".number").style.width = "15rem";
+    document.querySelector(".number").textContent = "?";
+    document.querySelector(".guess").value = "";
+    document.querySelector(".message").textContent = "Start guessing..."
+    score = 20;
+    document.querySelector(".score").textContent = score;
+    secretNumber = Math.trunc(Math.random() * 20) +1;
 });
