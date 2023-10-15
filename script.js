6e5900f820136a81ad34f3a8aca0ea61a6d396f4
William Fischdick <jwfbit@hotmail.com>
2023-10-14 22:09:09 -0300

    :recycle: Refatorando o código, removendo partes duplicadas.

--- script.js
+++ script.js
@@ -24,26 +24,16 @@ document.querySelector(".check").addEventListener("click", function() {
 
         }
 
-    } else if (guess > secretNumber) {
+    } else if (guess !== secretNumber) {
         if (score > 1) {
-            document.querySelector(".message").textContent = "Too High!";
+            document.querySelector(".message").textContent = guess > secretNumber ? "Too High!" : "Too Low!";
             score--;
             document.querySelector(".score").textContent = score;
         } else {
             document.querySelector(".message").textContent = "You Lost the Game!";
             document.querySelector(".score").textContent = 0;
         }
-
-
-    } else if (guess < secretNumber) {
-        if (score > 1) {
-            document.querySelector(".message").textContent = "Too Low!";
-            score--;
-            document.querySelector(".score").textContent = score;
-        } else {
-            document.querySelector(".message").textContent = "You Lost the Game!";
-            document.querySelector(".score").textContent = 0;
-        }
+        
     }
 });
 
