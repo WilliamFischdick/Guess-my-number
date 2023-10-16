0617474cd931baf49b9aac2facd7840c29dbb77c
William Fischdick <jwfbit@hotmail.com>
2023-10-15 21:41:41 -0300

    :recycle: Refatorando o código.

--- script.js
+++ script.js
@@ -4,16 +4,21 @@ let secretNumber = Math.trunc(Math.random() * 20) +1;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function (message) {
+    document.querySelector(".message").textContent = message;
+}
+
 document.querySelector(".check").addEventListener("click", function() {
     const guess = Number(document.querySelector(".guess").value);
 
     if(!guess) {
 
-        document.querySelector(".message").textContent = "No Number!";
+        
+        displayMessage("No Number!");
 
     } else if (guess === secretNumber) {
 
-        document.querySelector(".message").textContent = "Correct Number!";
+        displayMessage("Correct Number!");
         document.querySelector("body").style.backgroundColor = "#60b347";
         document.querySelector(".number").style.width = "30rem";
         document.querySelector(".number").textContent = secretNumber;
@@ -26,11 +31,11 @@ document.querySelector(".check").addEventListener("click", function() {
 
     } else if (guess !== secretNumber) {
         if (score > 1) {
-            document.querySelector(".message").textContent = guess > secretNumber ? "Too High!" : "Too Low!";
+            displayMessage(guess > secretNumber ? "Too High!" : "Too Low!");
             score--;
             document.querySelector(".score").textContent = score;
         } else {
-            document.querySelector(".message").textContent = "You Lost the Game!";
+             displayMessage("You Lost the Game!");
             document.querySelector(".score").textContent = 0;
         }
         
@@ -42,7 +47,7 @@ document.querySelector(".again").addEventListener("click", function() {
     document.querySelector(".number").style.width = "15rem";
     document.querySelector(".number").textContent = "?";
     document.querySelector(".guess").value = "";
-    document.querySelector(".message").textContent = "Start guessing..."
+    displayMessage("Start guessing...");
     score = 20;
     document.querySelector(".score").textContent = score;
     secretNumber = Math.trunc(Math.random() * 20) +1;
